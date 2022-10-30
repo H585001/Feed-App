fd75fbeb6eb2fc4ec9bc180188e87af0bafa4d56
h579699 <jjonasson@ymail.com>
2022-10-30 12:02:27 +0100

    Added missing files

---
+++ FeedApp-analytics/src/main/java/group4/feedapp/analytics/FeedAppAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package group4.feedapp.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedAppAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedAppAnalyticsApplication.class, args);
+	}
+
+}
---
+++ FeedApp-analytics/src/test/java/group4/feedapp/analytics/FeedAppAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.feedapp.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedAppAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
