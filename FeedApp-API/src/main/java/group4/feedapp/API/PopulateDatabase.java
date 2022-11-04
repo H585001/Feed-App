0f2f8d9f11e02adfbd5d256f368f7e3ba1638e91
h579699 <jjonasson@ymail.com>
2022-11-04 12:43:28 +0100

    Changed test data

--- FeedApp-API/src/main/java/group4/feedapp/API/PopulateDatabase.java
+++ FeedApp-API/src/main/java/group4/feedapp/API/PopulateDatabase.java
@@ -1,6 +1,9 @@
 package group4.feedapp.API;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -34,28 +37,18 @@ public class PopulateDatabase {
         
         em.getTransaction().begin();
         
-        // Test data
+        // Init data
+        Date start = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(start);
+        c.add(Calendar.DATE, 7);
+        Date end = c.getTime();
+        
 		FAUser user = new FAUser("simon@gmail.com", "test", "Simon", true);
-		Poll poll1 = new Poll("Are you sick and tired?", 0, 0, null, null, true, 0, "ABCDEF", user);
-		Poll poll2 = new Poll("Do you like America?", 0, 0, null, null, false, 0, "AMERICA", user);
+		Poll poll1 = new Poll("Are you sick and tired?", 0, 0, start, end, true, 0, "ABCDEF", user);
+		Poll poll2 = new Poll("Do you like America?", 0, 0, start, end, false, 0, "AMERICA", user);
 		IoTDevice iot1 = new IoTDevice("IoT Device 1", poll1);
 		IoTDevice iot2 = new IoTDevice("IoT Device 2", poll1);
-		
-		Vote votePoll1 = new Vote(user, poll1, true);
-		Vote votePoll2 = new Vote(user, poll2, false);
-		
-		user.getVotes().add(votePoll1);
-		user.getVotes().add(votePoll2);
-		
-		poll1.getUserVotes().add(votePoll1);
-		poll1.setYesCount(poll1.getYesCount() + 1);
-		poll2.getUserVotes().add(votePoll2);
-		poll2.setNoCount(poll2.getNoCount() + 1);
-		
-		IoTVotes iot1Votes = new IoTVotes(3, 4, iot1, poll1);
-		poll1.getIotVotes().add(iot1Votes);
-		poll1.setYesCount(poll1.getYesCount() + iot1Votes.getYesCount());
-		poll1.setNoCount(poll1.getNoCount() + iot1Votes.getNoCount());
 		
 		// Persisting
         em.persist(user);
@@ -63,9 +56,6 @@ public class PopulateDatabase {
         em.persist(poll2);
         em.persist(iot1);
         em.persist(iot2);
-        em.persist(votePoll1);
-        em.persist(votePoll2);
-        em.persist(iot1Votes);
         em.getTransaction().commit();
         
         em.close();
